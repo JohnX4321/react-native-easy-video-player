80ad05054753765d85b3bc2ce5c7538288010596
Samarth MS <johnx4321gaming@gmail.com>
2019-10-07 12:05:32 +0530

    Second Commit

--- App.js
+++ App.js
@@ -23,16 +23,10 @@ import {
   DebugInstructions,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+//import VideoComponent from './src/VideoComponent';
+
 import VideoComponent from './src/VideoComponent';
 
-export default class App  extends React.Component{
 
-  render(): boolean | number | string | React$Element<*> | React$Portal | Iterable | null {
-    return (
-        <VideoComponent videoWidth={640} videoHeight={640}
-        url={'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4'}
-        duration={634} title={'Easy Video Player'}/>
-    )
-  }
 
-}
+export default VideoComponent;
